793abf2f7035b3c6b038220e89dd0253fe763b03
Michael Imole Kolawole <dremkay71@gmail.com>
2022-10-13 06:47:19 -0700

    task 4 modified

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * the program loads the main function
- * thats the starting point.
+ * main - the program loads the main function, thats the starting point.
  *
- * ends the execution by returning a 
- * value. return(0)
- *
+ * return - ends the execution by returning a value
  */
-int main (void)
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
