8db773ae448d4ee93c41107db01bc61d901d65a8
Michael Imole Kolawole <dremkay71@gmail.com>
2022-10-14 13:44:23 -0700

    task 6 added(0-9 is 48-57 since we are using putchar)

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main - Program starts here o
+*
+* Return: Always Zero
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
