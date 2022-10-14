3aef429611715e89c0090ec11dcfbdeac1444441
Michael Imole Kolawole <dremkay71@gmail.com>
2022-10-14 13:28:23 -0700

    task 5 added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+* main program starts here
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
