5fdf7d444e3a0abcd3fa7b48f0df4101171a63ad
Michael Imole Kolawole <dremkay71@gmail.com>
2022-10-18 14:32:56 -0700

    task 1 added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+* main - Starts
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 97;
+	while (i <= 122)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
+	return (0);
+}
