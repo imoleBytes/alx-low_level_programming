b8399126280efca67bfcfdb9450abeaf66c77296
imole <dremkay71@gmail.com>
2023-07-19 00:43:41 +0100

    task 4 added

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+/**
+* _isalpha - starts here
+* @c: to be passed into func
+* Return: 1 || 0
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
