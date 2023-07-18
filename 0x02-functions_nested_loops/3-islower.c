d6f14658315b4d528ff3a7ce11d8d2466506cdfa
imole <dremkay71@gmail.com>
2023-07-19 00:18:57 +0100

    task 3 added

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+/**
+* _islower - starts here
+*@c: values to be compared
+* Return: 1 || 0
+*/
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
