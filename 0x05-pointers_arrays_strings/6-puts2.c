1f33649f5ed4d3d72595a1ac0a1cb2e41469bc48
imole <dremkay71@gmail.com>
2023-07-24 19:21:40 +0100

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+* puts2 - print alternate character
+* @str: string
+*/
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		if (s[i + 1] == '\0')
+			break;
+		i = i + 2;
+	}
+	_putchar('\n');
+}
