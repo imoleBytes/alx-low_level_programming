ff0bf1ef6a3b9f3b3dffaafb78498c14fc1b97c8
imole <dremkay71@gmail.com>
2023-07-24 14:41:26 +0100

    task 0 addede

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+
+/**
+* reset_to_98 - this reset the value of the variable to 98
+* @n: the variable address
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+pointers, arrays and strig taskes here
