5c78397c39ead5f2982cc6ef9bac923e0fc5ffb9
imole <dremkay71@gmail.com>
2023-07-24 19:37:21 +0100

    task7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include <main.h>
+
+
+/**
+* puts_half - print 2nd half of the string
+* @str: string
+*/
+void puts_half(char *str)
+{
+	int len = 0;
+	int i;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	i = (len / 2) - 1;
+
+	while (i < len)
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
