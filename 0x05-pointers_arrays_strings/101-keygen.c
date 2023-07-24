828ccd2a4edcd5d864bdf49cbac5f617e84f56f2
Imọlẹ <94703897+imoleBytes@users.noreply.github.com>
2023-07-24 20:58:33 +0100

    Task 11 finally

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,23 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-* main - generates keygen.
+* main - Generates a keygen.
 * Return: 0
 */
 
 int main(void)
 {
-	int i = 0, j = 0;
+	int id = 0, j = 0;
 	time_t t;
 
 	srand((unsigned int) time(&t));
 	while (j < 2772)
 	{
 		i = rand() % 128;
-		if ((j + i) > 2772)
+		if ((j + id) > 2772)
 			break;
-		j += i;
-		printf("%c", i);
+		j += id;
+		printf("%c", id);
 	}
 	printf("%c\n", (2772 - j));
 	return (0);
