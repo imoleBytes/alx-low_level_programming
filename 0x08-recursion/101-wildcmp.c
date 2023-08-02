bc84fbfacc6ff09e562273913c900d8e2d3bde04
imole <dremkay71@gmail.com>
2023-08-02 13:49:54 +0100

    101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,9 +1,9 @@
 
 
 /**
- * wildcmp - Compare strings
- * @s1: pointer to string params
- * @s2: pointer to string params
+ * wildcmp - Compare stringsss
+ * @s1: pointer to string
+ * @s2: pointer to string 2
  * Return: 0
  */
 
@@ -11,21 +11,14 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
 	{
-		
-if (*s2 != '\0' && *s2 == '*')
-{
-return (wildcmp(s1, s2 + 1));
-}
-return (*s2 == '\0');
-}
+		if (*s2 != '\0' && *s2 == '*')
+			return (wildcmp(s1, s2 + 1));
+		return (*s2 == '\0');
+	}
 
-if (*s2 == '*')
-{
-return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
-}
-else if (*s1 == *s2)
-{
-return (wildcmp(s1 + 1, s2 + 1));
-}
-return (0);
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	else if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
