d6bb22e00a0b2b28e0e018e3ab1839646041b744
imole <dremkay71@gmail.com>
2023-08-04 13:55:19 +0100

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - main func
+* @argc: arguments count
+* @argv: list of arguments
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
