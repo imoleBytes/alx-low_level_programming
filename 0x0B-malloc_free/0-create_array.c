6e514d536e48cb6b314ccc44373163a2b116e9b9
imole <dremkay71@gmail.com>
2023-08-07 20:53:47 +0100

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+
+
+/**
+* create_array - func to create a block of memmory and fill
+* @size: array size
+* @c: character to fill
+* Return: a pointer to array
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int *array;
+	int i;
+
+	array = malloc(sizeof(*array) * size);
+
+	if (array != NULL && size != 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			array[i] = c;
+		}
+
+		return (array);
+	}
+
+	return (NULL);
+}
