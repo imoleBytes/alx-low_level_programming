1cd7ba5e50fba5ed70e362b00cbb88c82dbf0bef
imole <dremkay71@gmail.com>
2023-08-07 21:14:06 +0100

    task1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+
+
+
+/**
+* create_array - func to create a block of memmory and fill it with string
+* @str: array size
+* Return: a pointer to array
+*/
+
+char *_strdup(char *str)
+{
+
+	char *array;
+	int i;
+	
+
+	array = malloc(sizeof(*str));
+
+	if (array != NULL)
+	{
+		// for (i = 0; i < size; i++)
+		while (str[i] != '\0')
+		{
+			array[i] = str[i];
+		}
+
+		return (array);
+	}
+
+	return (NULL);
+}
