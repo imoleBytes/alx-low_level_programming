a20992fee17074b824c22df92ada534b1e34679f
imole <dremkay71@gmail.com>
2023-08-14 16:33:08 +0100

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _dog_h
+#define _dog_h
+
+
+
+/**
+* struct dog - a dog type
+* @name: char name of dog
+* @age: age of dog in float
+* @owner: string, owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
