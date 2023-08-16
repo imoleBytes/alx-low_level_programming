c5cf96e37875a791e2159f7073fca5cc0f8cf854
imole <dremkay71@gmail.com>
2023-08-16 10:36:05 +0100

    task2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+
+/**
+* int_index - func starts
+* @array: array of int
+* @size: int size of arr
+* @cmp: func pointer
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	int value;
+
+	if (!array || !cmp)
+		return (-1);
+
+	if (size <= 0)
+		return (-1);
+	while (i < size)
+	{
+		value = cmp(array[i]);
+		if (value != 0)
+		{
+			return (i);
+		}
+		i++;
+	}
+	return (-1);
+}
