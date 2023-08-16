74eb401c43eb82f65b138ed538fa34a7edb66656
imole <dremkay71@gmail.com>
2023-08-16 12:23:57 +0100

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,6 +12,7 @@
 int main (int argc, char *argv[])
 {
 	int num1, num2, res;
+	char op;
 	int (*func)(int, int);
 
 	if (argc != 4)
@@ -21,6 +22,14 @@ int main (int argc, char *argv[])
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
+
+	op = *argv[2];
+
+	if ((op == '/' || op == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 
 	func = get_op_func(argv[2]);
 
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 
 /**
 * op_add - func returns the addition of a and b.
@@ -47,11 +45,6 @@ int op_mul(int a, int b)
 
 int op_div(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a / b);
 }
 
@@ -65,10 +58,5 @@ int op_div(int a, int b)
 
 int op_mod(int a, int b)
 {
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
 	return (a % b);
 }
