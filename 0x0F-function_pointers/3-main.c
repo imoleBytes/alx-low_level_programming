94d46966882ce17da6269e4ee4b3a06027ecef1a
imole <dremkay71@gmail.com>
2023-08-16 12:10:10 +0100

    task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+
+
+
+int main (int argc, char *argv[])
+{
+	int num1, num2, res;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	func = get_op_func(argv[2]);
+
+	res = func(num1, num2);
+	printf("%d\n", res);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* op_add - func returns the addition of a and b.
+* @a: int a
+* @b: int b
+* Return: int result
+*/
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* op_sub - func returns the difference of a and b.
+* @a: int a
+* @b: int b
+* Return: int
+*/
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* op_mul - func returns the product of a and b.
+* @a: int a
+* @b: int b
+* Return: int
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+* op_div - func returns the division of a and b.
+* @a: int a
+* @b: int b
+* Return: int
+*/
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+
+/**
+* op_mod - func returns the modular of a and b.
+* @a: int a
+* @b: int b
+* Return: int
+*/
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
