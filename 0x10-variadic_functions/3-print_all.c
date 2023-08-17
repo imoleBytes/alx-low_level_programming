faff36bb0343ccf81ba3b9a994323292e0fdf58b
imole <dremkay71@gmail.com>
2023-08-17 18:09:18 +0100

    task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_all - func to prints all
+ * @format: string
+ */
+void print_all(const char * const format, ...)
+{
+	va_list arguments;
+	unsigned int i = 0;
+	unsigned int j;
+	unsigned int k = 0;
+	char *_strings;
+	const char argTypes[] = "cifs";
+
+	va_start(arguments, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (argTypes[j])
+		{
+			if (format[i] == argTypes[j] && k)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(arguments, int)), k = 1;
+			break;
+		case 'i':
+			printf("%d", va_arg(arguments, int)), k = 1;
+			break;
+		case 'f':
+			printf("%f", va_arg(arguments, double)), k = 1;
+			break;
+		case 's':
+			_strings = va_arg(arguments, char *), k = 1;
+			if (!_strings)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", _strings);
+			break;
+		} i++;
+	}
+	printf("\n"), va_end(arguments);
+}
