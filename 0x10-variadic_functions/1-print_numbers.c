1db94fa8f9edcad5ca99a61af0b8c1191acbf6dc
imole <dremkay71@gmail.com>
2023-08-17 17:16:38 +0100

    task1 o

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,14 +11,14 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list numbers;
 	unsigned int i;
 
-	
+
 
 	va_start(numbers, n);
 
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(numbers, int));
-		if (separator && i < n-1)
+		if (separator && i < (n - 1))
 			printf("%s", separator);
 	};
 	printf("\n");
