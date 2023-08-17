625340dcd1f384c79ce9f81b482e8e58eed2d69c
imole <dremkay71@gmail.com>
2023-08-17 17:40:14 +0100

    task2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+
+/**
+* print_strings - func to print all args
+* @separator: const char pointer
+* @n: int
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list strrs;
+	unsigned int i;
+	char *temp;
+
+	va_start(strrs, n);
+
+	for (i = 0; i < n; i++)
+	{
+		strrs = va_arg(strrs, char *);
+		if (temp)
+		{
+			printf("%s", temp);
+		}
+		else
+		{
+			printf("(nil)");
+		}
+			
+		if (separator && i < (n - 1))
+			printf("%s", separator);
+	};
+	printf("\n");
+
+	va_end(strrs);
+}
