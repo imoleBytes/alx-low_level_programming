86f62daf27a43c3b260120079036ec2df265eb2d
imole <dremkay71@gmail.com>
2023-09-07 17:12:17 +0100

    task1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+#define PERMISION 0600
+
+/**
+* create_file - func to create a file.
+* @filename: name of file.
+* @text_content: content to write.
+* Return: 1 if it success. -1 if it fails.
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int content_lenght;
+	int w;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, PERMISION);
+
+	if (fd == -1)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	for (content_lenght = 0; text_content[content_lenght]; content_lenght++)
+		;
+
+	w = write(fd, text_content, content_lenght);
+
+	if (w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
