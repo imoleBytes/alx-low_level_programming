5763ec55d7f8294a330b73ac676fe8ea47544b87
imole <dremkay71@gmail.com>
2023-11-02 18:39:00 +0100

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,23 +14,24 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *newHt;
 	hash_node_t **arr;
 
-	arr = (hash_node_t **)malloc(sizeof(hash_node_t) * size);
+	arr = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
 
 	if (arr == NULL)
 	{
-		return ((hash_table_t)NULL);
+		return (NULL);
 	}
 
 	newHt = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (newHt == NULL)
 	{
-		return ((hash_table_t)NULL);
+		return (NULL);
 	}
 
 	newHt->size = size;
 
 	newHt->array = arr;
+
 
 	return (newHt);
 }
