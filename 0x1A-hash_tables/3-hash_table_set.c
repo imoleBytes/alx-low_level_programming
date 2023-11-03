a9837af7ac08f476080ac140f0cecaa6398f0f99
imole <dremkay71@gmail.com>
2023-11-03 14:42:31 +0100

    task3 o

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -47,8 +47,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
-		ht->array[index] = new_node;
 		new_node->next = head;
+		ht->array[index] = new_node;
+		
 		return (1);
 	}
 	return (0);
