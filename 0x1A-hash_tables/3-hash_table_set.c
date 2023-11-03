b5456424e7e42a4228c4e5e9d11995bb5ec0757f
imole <dremkay71@gmail.com>
2023-11-03 14:56:12 +0100

    task3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -18,7 +18,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *head;
 
-	if (key == NULL || strlen(key) == 0)
+	/*if (key == NULL || strlen(key) == 0)*/
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
 	new_node = malloc(sizeof(hash_node_t));
