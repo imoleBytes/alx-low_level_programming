240ef6dc0a4470f5e85d181a3370e1fd4073c45b
imoleFolio <dremkay71@gmail.com>
2024-03-11 12:46:15 +0100

    task 0: betty complied

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,9 +10,11 @@
 */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i;
+
 	if (array)
 	{
-		for (size_t i = 0; i < size; i++)
+		for (i = 0; i < size; i++)
 		{
 			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 			if (array[i] == value)
